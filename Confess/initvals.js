d394194971c33cd37b7959dd990cfce0a5f85a47
Johnny <johnny@brooklynresearch.org>
2015-04-19 16:27:02 -0400

    Confessional load

---
+++ Confess/initvals.js
@@ -0,0 +1,26 @@
+// initvals.js 
+// values shared in other files
+
+// change these guys
+var debug = 0;
+var win7 = 0;
+var eventFolder = 'confession';
+var socket = 8081;
+
+// this gets us the correct base folders based on debug, os, date and event
+var fdate = new Date();
+Date.prototype.addHours = function(h){
+    this.setHours(this.getHours()+h);
+    return this;
+}
+var fdatestring = ("0" + (fdate.getMonth() + 1).toString()).substr(-2) + "_" + ("0" + fdate.getDate().toString()).substr(-2)  + "_" + (fdate.getFullYear().toString()).substr(2);
+var nasBasePath = ['/Volumes/OCULTO/'+fdatestring+'/'+eventFolder, 'Z:/'+fdatestring+'/'+eventFolder];
+var debugBasePath = __dirname + '/images/'+fdatestring+'/'+eventFolder;
+var basePath = [nasBasePath[win7], debugBasePath];
+
+module.exports = {
+	debug: debug,
+	basePath: basePath,
+	eventFolder: eventFolder,
+	socket: socket
+};
---
+++ Confess/public/js/rapper.js
@@ -0,0 +1,41 @@
+// rapsheet folder
+var dateOfIncident;
+var activities = ['Strange browser activity',
+	'Loud music playing',
+	'Bad music playing',
+	'Excessive flirting',
+	'Waking up the neighbors',
+	'Party fouls',
+	'Sleeping all day, partying all night',
+	'Inability to respect personal space',
+	'Overzealous high fiving',
+	'Prolonged after-the-fact laughter',
+	'Luring friends into the unknown',
+	'Spontaneous salacious activity',
+	'Aggressive thumb wars',
+	'Enthusiastic party food consumption',
+	'Excessive bad TV consumption',
+	'Extravagant use of hairspray',
+	'Wearing sunglasses at night',
+	'Popping too many collars',
+	'Enjoying NSFW material at work',
+	'Extreme use of emoji',
+	'Using “LOL” in every text message',
+	'Forgetting to put down the toilet seat', 
+	'Fear of velvet',
+	'Secret love for boy bands',
+	'Sleeping too much',
+	'Not sleeping enough',
+	'Incessant guessing of friends’ spirit animals',
+	'Preoccupation with pickles',
+	'Constant desire to arm wrestle',
+	'Discreetly sweaty armpits',
+	'Love of comic sans',
+	'Recurring dreams featuring burritos',
+	'Misusing the phrase “on fleek”',
+	'Extreme couponing'];
+
+function generateRapSheet(){
+	
+	
+}

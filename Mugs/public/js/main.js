a25a4aff7a6ec119807c0bbe4fd9dd11ccbbd9a9
Johnny <johnny@brooklynresearch.org>
2015-04-19 19:00:47 -0400

    added new list of suspicious activities
    add

--- Mugs/public/js/main.js
+++ Mugs/public/js/main.js
@@ -235,18 +235,40 @@ function createRapsheet(){
 	rapcontext.fillText("XXXXXXX", docWidth*0.75+100, 4*resolution);
 
 	//list of possible offences
-	var OffenceLib = [
-		"Strange Browser Activity",
-		"Loud Music Playing",
-		"Bad Music Playing",
-		"Waking Up the Neighbors",
-		"Excessive Flirting",
-		"Indecent Exposure",
-		"Dancing Suggestively",
-		"Improper Use of a Public Parking Meter",
-		"Jaywalking",
-		"Sleeping in a Public Place"
-		];
+	var OffenceLib = ['Strange browser activity',
+	'Loud music playing',
+	'Bad music playing',
+	'Excessive flirting',
+	'Waking up the neighbors',
+	'Party fouls',
+	'Sleeping all day, partying all night',
+	'Inability to respect personal space',
+	'Overzealous high fiving',
+	'Prolonged after-the-fact laughter',
+	'Luring friends into the unknown',
+	'Spontaneous salacious activity',
+	'Aggressive thumb wars',
+	'Enthusiastic party food consumption',
+	'Excessive bad TV consumption',
+	'Extravagant use of hairspray',
+	'Wearing sunglasses at night',
+	'Popping too many collars',
+	'Enjoying NSFW material at work',
+	'Extreme use of emoji',
+	'Using “LOL” in every text message',
+	'Forgetting to put down the toilet seat', 
+	'Fear of velvet',
+	'Secret love for boy bands',
+	'Sleeping too much',
+	'Not sleeping enough',
+	'Incessant guessing of friends’ spirit animals',
+	'Preoccupation with pickles',
+	'Constant desire to arm wrestle',
+	'Discreetly sweaty armpits',
+	'Love of comic sans',
+	'Recurring dreams featuring burritos',
+	'Misusing the phrase “on fleek”',
+	'Extreme couponing'];
 		
 	//create a unique random number between 0-OffenceLib length
 	var uniqueRandoms = [];

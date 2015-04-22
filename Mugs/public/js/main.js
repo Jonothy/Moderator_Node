ed718561643929f900c44c974d58188af34ca391
Johnny <johnny@brooklynresearch.org>
2015-04-22 13:09:44 -0400

    Rapsheet draw
    new noise assets

--- Mugs/public/js/main.js
+++ Mugs/public/js/main.js
@@ -385,16 +385,17 @@ $("#data-submit").submit(function(e)
 	saveImageData = hiddencontext.getImageData(0,0, hiddencanvas.width, hiddencanvas.height);
 	var hidden_cb_image = Filters.brightnessContrast(saveImageData, parseFloat(document.getElementById('brightness-bar').value) / 10.0, parseFloat(document.getElementById('contrast-bar').value) / 10.0);
 	hiddencontext.putImageData(hidden_cb_image, 0, 0);
-	// draw border
 	hiddencontext.drawImage(hidBorder, 0, 0);
 	// draw noise
 	hiddencontext.drawImage(hidNoise, 0, 0);
+	var canvasData = hiddenCanvas.toDataURL("image/jpeg");
+	// draw border
+	
     // hiddencontext.globalCompositeOperation = "source-over";
 	// hiddencontext.drawImage(compositeObj,-100,-10);
 
     var postData = 'photo=' + document.getElementById('photo-name').value;
     // console.log(postData);
-    var canvasData = hiddenCanvas.toDataURL("image/jpeg");
     var rapData = rapCanvas.toDataURL("image/jpeg", 1.0);
 	postData += "&imgData=";
 	postData += canvasData.replace(/^data:image\/(jpg|jpeg);base64,/, "");
